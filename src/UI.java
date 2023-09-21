588ef5de709c560ab93d392f5978a9f2d329494e
arjunbalaji99 <91145107+arjunbalaji99@users.noreply.github.com>
2023-09-21 20:37:16 +0000

    ui testing

---
+++ src/UI.java
@@ -0,0 +1,28 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+// video i am using - https://www.youtube.com/watch?v=5o3fMLPY7qY
+
+public class UI {
+    public UI() {
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+        panel.setLayout(new GridLayout(0, 1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Calculator");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new UI();
+    }
+}
